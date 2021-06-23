dd8043a1c0efe670b49c8008a4dc143c0a41abe5
Mariapaz Silva <mariap.silvag@duocuc.cl>
2021-06-23 02:34:26 -0400

    Diccionario de contexto con objetos

--- core/views.py
+++ core/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+        super().__init__()
+
 # Create your views here.
 def home (request):
     lista = ["Chocolate", "Protos con Masamorra", "Cazuela"]
+
+    hijo = Persona ("Valentin Olivares", '2')
+
     contexto = {
         'nombre' :'Mariapaz Silva',
         'edad':23,
-        'comidas': lista
+        'comidas': lista,
+        'hijo': hijo 
     }
     return render(request,'core/home.html', contexto)
